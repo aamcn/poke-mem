3ea7646694ea2728f0d3ef986c567c2065485cae
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-02 15:45:30 +0100

    Feat: Import Game component into GamePage, add gamePage css module

--- src/pages/gamePage/GamePage.tsx
+++ src/pages/gamePage/GamePage.tsx
@@ -1,10 +1,10 @@
-
+import Game from '../../components/gameComponents/game/Game';
 
 function GamePage() {
   
   return (
     <div data-testid="game-page">
-     
+      <Game />
     </div>
   );
 }
