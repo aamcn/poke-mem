8c799ad06917c1024fbdce1e9d65f4f5d9e7a962
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-02 20:07:58 +0100

    Feat: Add further useReduce actions and there functions

--- src/components/gameComponents/game/Game.tsx
+++ src/components/gameComponents/game/Game.tsx
@@ -1,11 +1,39 @@
-import { useMemo, useState } from "react";
 import GameMenu from "../gameMenu/GameMenu";
 import { useContext, createContext, useReducer} from "react";
 
-interface Actions {
-  type: "toggleGameStarted" | "toggleGameDifficulty";
-  payload: boolean | string;
+interface ToggleGameStartedAction {
+  type: "toggleGameStarted" 
+  payload: boolean;
 }
+
+interface ToggleGameDifficultyAction {
+  type: "toggleGameDifficulty"
+  payload: [string, number];
+}
+
+interface ToggleGameWonAction {
+  type: "toggleGameWon"
+  payload: boolean;
+}
+
+interface ToggleGameLostAction {
+  type: "toggleGameLost"
+  payload: boolean;
+}
+
+interface IncrementScoreAction {
+  type: "incrementScore"
+  payload: null;
+}
+
+interface ResetGameAction {
+  type: "resetGame"
+  payload: null;
+}
+
+type AppActions = ToggleGameStartedAction | ToggleGameDifficultyAction | ToggleGameWonAction | ToggleGameLostAction | IncrementScoreAction | ResetGameAction;
+
+// Define the shape of the game state
 
 interface gameObjectState {
   gameStarted: boolean;
@@ -16,15 +44,14 @@ interface gameObjectState {
   score: number;
 }
 
-
 // Reducer function to manage object state transitions
-function gameReducer(state: gameObjectState, action: Actions) {
+function gameReducer(state: gameObjectState, action: AppActions) {
   const {type, payload} = action;
   switch (type) {
     // Change the game started boolean value.
     case "toggleGameStarted":
       return {
-        gameStarted: !state.gameStarted,
+        gameStarted: payload as boolean,
         gameWon: state.gameWon,
         gameLost: state.gameLost,
         gameDifficulty: state.gameDifficulty,
@@ -37,10 +64,50 @@ function gameReducer(state: gameObjectState, action: Actions) {
           gameStarted: !state.gameStarted,
           gameWon: state.gameWon,
           gameLost: state.gameLost,
-          gameDifficulty: payload as string,
-          cardTotal: state.cardTotal,
+          gameDifficulty: payload[0] as string,
+          cardTotal: payload[1] as number,
           score: state.score,
       };
+      // Change the game won boolean value.
+      case "toggleGameWon":
+        return {
+          gameStarted: !state.gameStarted,
+          gameWon: payload as boolean,
+          gameLost: state.gameLost,
+          gameDifficulty: state.gameDifficulty,
+          cardTotal: state.cardTotal,
+          score: state.score,
+        };
+      // Change the game lost boolean value.
+      case "toggleGameLost":
+        return {
+          gameStarted: !state.gameStarted,
+          gameWon: payload as boolean,
+          gameLost: state.gameLost,
+          gameDifficulty: state.gameDifficulty,
+          cardTotal: state.cardTotal,
+          score: state.score,
+        };
+        // Increment the score by 1.
+        case "incrementScore":
+        return {
+          gameStarted: !state.gameStarted,
+          gameWon: state.gameWon,
+          gameLost: state.gameLost,
+          gameDifficulty: state.gameDifficulty,
+          cardTotal: state.cardTotal,
+          score: state.score + 1,
+        };
+        // Reset the game to initial state.
+        case "resetGame":
+        return {
+          gameStarted: false,
+          gameWon: false,
+          gameLost: false,
+          gameDifficulty: 'Easy',
+          cardTotal: 4,
+          score: 0,
+        };
     default:
       throw new Error("Unknown action type");
   }
@@ -49,10 +116,11 @@ function gameReducer(state: gameObjectState, action: Actions) {
 
 export interface GameContextType {
    state: gameObjectState;
-   dispatch: React.Dispatch<Actions>;
+   dispatch: React.Dispatch<AppActions>;
 }
 
 const GameContext = createContext<GameContextType>({} as GameContextType);
+
 
 export const useGameContext = () => {
   const context = useContext(GameContext);
@@ -62,14 +130,14 @@ export const useGameContext = () => {
   return context;
 }
 
-
 function Game() {
-  const [state, dispatch] = useReducer(gameReducer, {gameStarted: false, gameWon: false, gameLost: false, gameDifficulty: "easy", cardTotal: 0, score: 0});
+  const [state, dispatch] = useReducer(gameReducer, {gameStarted: false, gameWon: false, gameLost: false, gameDifficulty: "easy", cardTotal: 4, score: 0});
 
 const gameContextValue: GameContextType = {
   state,
   dispatch,
 }
+
     return (
         <>
         <h1>Game Component</h1>
