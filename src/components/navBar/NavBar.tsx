61ba9ee213c7be0a86247b25bdd2da9ecb0f2c04
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-02 14:32:16 +0100

    Feat: Add NavBar component and create and import its css module

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,27 @@
+
+import { Link } from "react-router-dom";
+import styles from "./navBar.module.css";
+
+function Navbar() {
+  return (
+    <nav
+      data-testid="navbar"
+      className={styles.navbar}
+      id="navbar"
+      aria-label="navbar"
+    >
+      <Link id="game-link" to="/game-page" aria-label="Game Page Link">
+        Game
+      </Link>
+      <Link
+        id="leaderboard-link"
+        to="/leader-board"
+        aria-label="Leader Board Link"
+      >
+        Leader Board
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
