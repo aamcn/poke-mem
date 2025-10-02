f7a15dd17de70f4da527b69bc2d20a08a25eeaec
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-02 14:32:53 +0100

    Feat: Create Header component and its css module, import NavBar into Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,34 @@
+
+import styles from "./header.module.css";
+import { Link } from "react-router-dom";
+import Navbar from "../navBar/navBar";
+
+function Header() {
+  return (
+    <header
+      className={styles.headerContainer}
+      data-testid="header"
+      aria-label="header"
+    >
+      <Navbar />
+      <div
+        className={styles.titleContainer}
+        data-testid="header-title-container"
+      >
+        <h1 aria-label="Game Title: Poke-Mem">
+          <Link
+            data-testid="header-title"
+            id="game-title"
+            className={styles.gameTitle}
+            aria-label="Game Title: Poke-Mem"
+            to="/game-page"
+          >
+            Poke-Mem
+          </Link>
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
