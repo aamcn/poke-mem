547d6e1d2a4e41e1047b5fce9ad702a3c007f5e5
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-03 18:40:30 +0100

    Feat: Setup component to import props and the elements to display the data.

---
+++ src/components/cardComponents/cardTemplate/CardTemplate.tsx
@@ -0,0 +1,44 @@
+import type PokemonCardObject from "./cardConstructor/cardConstructor";
+import { useGameContext } from "../../gameComponents/game/Game";
+import styles from "./cardTemplate.module.css";
+import React, { useMemo } from "react";
+
+interface CardTemplateProps {
+  key?: number; // Optional key prop for list rendering
+  cardDetails: PokemonCardObject; // Make this optional to avoid errors if not provided
+}
+
+function CardTemplate({ cardDetails }: CardTemplateProps) {
+  const { state, dispatch } = useGameContext();
+  /* 
+    When the card is clicked, if it is the first time, the isClicked state is 
+    changed to 'true' and a point is added to the current score. 
+    If the card has been previously clicked (meaning isClicked is already true) gameResults
+    is set to true ending the game which renders the gameOver pop up menu component.
+  */
+
+
+  return (
+    <div
+      key={cardDetails.id}
+      data-testid="playingCard"
+      className={cardClassSize[0]}
+      onClick={handleCardClick}
+      aria-label={`Click to select ${cardDetails.name} card`}
+    >
+      <div className={cardClassSize[1]}>
+        <img
+          className={styles.cardImage}
+          src={cardDetails.imageUrl}
+          alt={`${cardDetails.name} Pokemon`}
+          data-testid="card-image"
+        />
+      </div>
+      <div className={styles.cardTitleContainer}>
+        <p data-testid="card-name">{cardDetails.name}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CardTemplate;
