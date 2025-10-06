158b4704bc171877a0ee8831068c7986481ebc70
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-06 16:06:54 +0100

    Feat: users highScore is stored in localstorage and update if current score exceeds highscore

--- src/components/gameComponents/scoreBoard/scoreBoard.tsx
+++ src/components/gameComponents/scoreBoard/scoreBoard.tsx
@@ -1,15 +1,27 @@
 import { useGameContext } from "../../gameComponents/game/Game";
 import styles from "./scoreBoard.module.css";
 
+
+
 /* 
 Todo:
-  - Add high score functionality.
   - Move scoreboard to container at top of screen.
 */
 
 function ScoreBoard() {
   // Access state and dispatch from the game context
   const { state } = useGameContext();
+
+
+  function updateHighScore() {
+    if(localStorage.getItem('highScore') === null) {
+    localStorage.setItem('highScore', '0')
+    }
+  }
+  updateHighScore()
+  if(state.score > Number(localStorage.getItem('highScore'))) {
+    localStorage.setItem('highScore', state.score.toString())
+  }
 
   return (
     <div
@@ -26,7 +38,7 @@ function ScoreBoard() {
         <p className={styles.highScore} aria-label="High Score">
           High Score:
         </p>
-        <p data-testid="high-score-text">{state.highScore}</p>
+        <p data-testid="high-score-text">{localStorage.getItem('highScore')}</p>
       </div>
     </div>
   );
