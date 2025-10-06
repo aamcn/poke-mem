149396f5d94fd3b27259acef7cc54ce610855359
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-06 16:50:23 +0100

    Feat: Create LeaderBoardControls and its CSS module

---
+++ src/components/leaderBoardComponents/leaderBoardControls/LeaderBoardControls.tsx
@@ -0,0 +1,13 @@
+import styles from "./LeaderBoardControls.module.css";
+
+function LeaderBoardControls() {
+
+    return (
+        <div>
+            <h1>Leader Board Controls</h1>
+        </div>
+    )
+
+}
+
+export default LeaderBoardControls;
