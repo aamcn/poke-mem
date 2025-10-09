a5a991f3ca79eeff25c985c9f69f0625a3c41b93
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-09 12:46:02 +0100

    bug: Last apiUrl not being generated and added to the api array.

--- src/components/gameComponents/modules/fetchAllPokemonObjects/fetchAllPokemonObjects.ts
+++ src/components/gameComponents/modules/fetchAllPokemonObjects/fetchAllPokemonObjects.ts
@@ -1,19 +1,19 @@
-
 const apiUrlPrefix = "https://pokeapi.co/api/v2/pokemon/";
 
 function generatePokemonUrls() {
-    const pokemon: Array<object> = [];
-    for (let i = 1; i < 150; i++) {
-      // Generate the API URL for each Pokemon
-      const pokeUrl = apiUrlPrefix + i;
-      // Fetch the Pokemon data from the API
-      fetch(pokeUrl, { mode: "cors" })
-        .then((response) => response.json())
-        // Push the fetched data into the pokemon array
-        .then((response) => pokemon.push(response))
-        .catch((error) => console.error(error));
-    }
-    return pokemon;
+  const pokemon: Array<object> = [];
+  for (let i = 1; i <= 151; i++) {
+    // Generate the API URL for each Pokemon
+    const pokeUrl = apiUrlPrefix + i;
+    // Fetch the Pokemon data from the API
+    fetch(pokeUrl, { mode: "cors" })
+      .then((response) => response.json())
+      // Push the fetched data into the pokemon array
+      .then((response) => pokemon.push(response))
+      .catch((error) => console.error(error));
   }
+
+  return pokemon;
+}
 
 export { generatePokemonUrls };
