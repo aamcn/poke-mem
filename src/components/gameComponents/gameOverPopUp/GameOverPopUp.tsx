87769eb0ef19cafa7962cf7a4ad201ab018d4096
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-09 16:22:09 +0100

    Bug: Remove css properties causing app to lose shape when screen size is adjust to fringe case sizes

--- src/components/gameComponents/gameOverPopUp/GameOverPopUp.tsx
+++ src/components/gameComponents/gameOverPopUp/GameOverPopUp.tsx
@@ -7,31 +7,33 @@ function GameOverPopUp() {
 
   return (
     <div className={styles.lostGameWindow} data-testid="game-over-popup">
-      <h3
-        className={styles.gameOverTitle}
-        data-testid="game-over-title"
-        aria-label="Game Over"
-      >
-        Game Over
-      </h3>
-      <div className={styles.gameOverText}>
-        <p aria-label="Oh no...">Oh no...</p>
-        <p aria-label="You already clicked on that">
-          You already clicked on that
-        </p>
-        <br></br>
-        <p aria-label="Would you like to try again?">
-          Would you like to try again?
-        </p>
-      </div>
-      <div>
-        <button
-          className={styles.retryButton}
-          onClick={() => dispatch({ type: "resetGame", payload: null })}
-          aria-label="Retry Button"
+      <div className={styles.gameOverPopUp}>
+        <h3
+          className={styles.gameOverTitle}
+          data-testid="game-over-title"
+          aria-label="Game Over"
         >
-          Retry?
-        </button>
+          Game Over
+        </h3>
+        <div className={styles.gameOverText}>
+          <p aria-label="Oh no...">Oh no...</p>
+          <p aria-label="You already clicked on that">
+            You already clicked on that
+          </p>
+          <br></br>
+          <p aria-label="Would you like to try again?">
+            Would you like to try again?
+          </p>
+        </div>
+        <div>
+          <button
+            className={styles.retryButton}
+            onClick={() => dispatch({ type: "resetGame", payload: null })}
+            aria-label="Retry Button"
+          >
+            Retry?
+          </button>
+        </div>
       </div>
     </div>
   );
