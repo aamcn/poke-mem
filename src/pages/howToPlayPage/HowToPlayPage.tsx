2b5b52e788fdee78fee8e469048e223c5ca80357
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-13 12:27:53 +0100

    Feat: Create HowToPlayPage component and import child components

---
+++ src/pages/howToPlayPage/HowToPlayPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/header/Header";
+import InstructionsContainer from "../../components/howToPlayComponents/instructionsContainer/InstructionsContainter";
+
+function HowToPlayPage() {
+  return (
+    <div>
+      <Header />
+      <InstructionsContainer />
+    </div>
+  );
+}
+
+export default HowToPlayPage;
