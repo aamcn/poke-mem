4563f1950b0b179348a4c7db4f9ddba80bc572a3
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-13 12:24:50 +0100

    Feat: Update Routes.tsx to include path to 'how to play' page.

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,6 +3,7 @@ import LandingPage from "../pages/landingPage/LandingPage";
 import GamePage from "../pages/gamePage/GamePage";
 import RouterErrorPage from "../pages/routerErrorPage/RouterErrorPage";
 import LeaderBoardPage from "../pages/leaderBoardPage/LeaderBoardPage";
+import HowToPlayPage from "../pages/howToPlayPage/HowToPlayPage"; 
 
 const routes = [
   {
@@ -13,6 +14,7 @@ const routes = [
       { index: true, element: <LandingPage /> },
       { path: "/game-page", element: <GamePage /> },
       { path: "/leader-board", element: <LeaderBoardPage /> },
+      { path: "/how-to-play", element: <HowToPlayPage /> },
     ],
     errorElement: <RouterErrorPage />,
   },
